1e628d35c07ef6c4a2c16cee2cd0096dbad63f13
Mateus Gonçalves <89553011+Mateusgc01@users.noreply.github.com>
2023-11-29 14:44:07 +0000

    request methods partition

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,20 +1,27 @@
 package server;
 
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.*;
 
 public class Server {
-    final String ip;
+    // final String ip;
     final int port;
 
-    public Server(String ip, int port) {
-        this.ip = ip;
+    public Server(int port) {
         this.port = port;
     }
+   /*  public Server(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    } */
     
     public void run() {
         //IP da máquina na rede no host do INetAddress.getByName
-        try(ServerSocket httpServer = new ServerSocket(this.port, 0, InetAddress.getByName(this.ip))) {
+        // try(ServerSocket httpServer = new ServerSocket(this.port, 0, InetAddress.getByName(this.ip)))
+        try(ServerSocket httpServer = new ServerSocket(this.port)) {
             System.out.println("Aguardando requisiçoes...");
 
             while (true) {
@@ -37,14 +44,31 @@ public class Server {
                 request.append(line).append("\r\n");
             }
 
+            String[] requestSplitted = request.toString().split(" ");
+            String response = "";
+
+            switch (requestSplitted[0]) {
+                case "GET":
+                    response = this.getRequest();
+                    break;
+
+                case "PUT":
+                    
+                    break;
+            
+                case "POST":
+                    
+                    break;
+
+                case "DELETE":
+                    
+                    break;
+                default:
+                    throw new Exception("Requisição Inválida");
+                
+            }
             // Exibir a requisição no console
             System.out.println("Requisição:\n" + request.toString());
-
-            // Resposta HTTP de exemplo
-            String response = "HTTP/1.1 200 OK\r\n"
-                    + "Content-Type: text/plain\r\n"
-                    + "\r\n"
-                    + "Hello, World!\r\n";
 
             // Enviar a resposta ao cliente
             out.write(response.getBytes());
@@ -53,6 +77,29 @@ public class Server {
             clientSocket.close();
         } catch (Exception e) {
             System.out.println(e);
+        }
+    }
+    private String getRequest(){
+        // Resposta HTTP de exemplo
+        return sendFileContents("src/index.html");
+    }
+
+    private String sendFileContents(String fileName) {
+        try {
+            // Load the content of the HTML file
+            Path filePath = Paths.get(fileName);
+            byte[] fileBytes = Files.readAllBytes(filePath);
+
+            String page = new String(fileBytes);
+            // Send the file content
+            return "HTTP/1.1 200 OK\r\n"
+                    + "Content-Type: text/html\r\n"
+                    + "\r\n"
+                    + page;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
         }
     }
 }

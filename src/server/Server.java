6ccf9047c16c5b01894db0208d728d4d47411d01
Bruno <brunorobertolrr@gmail.com>
2023-12-05 01:37:06 -0300

    Acréscimo de um novo parâmetro de inicialização: nome da pasta root

--- src/server/Server.java
+++ src/server/Server.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 import java.io.*;
 
 public class Server {
